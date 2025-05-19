6eda48e8efa0cd2a6f7aca1e07c1eeaa7176b3ef
Arnav <arnavchaturvedi0@gmail.com>
2025-05-19 22:23:12 +0530

    update code in sidebar

--- server.js
+++ server.js
@@ -129,5 +129,5 @@ app.post('/submit-form', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
